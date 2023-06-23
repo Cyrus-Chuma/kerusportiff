40a25fc7e7ba94d5aad3c8fb16100889211eb126
kerugoyasportiff <cyrhowseven30@gmail.com>
2023-06-23 09:22:57 +0300

    changes

--- toggle.js
+++ toggle.js
@@ -1,4 +1,7 @@
 function toggleNavbar() {
     var navbarMenu = document.getElementById("navbarMenu");
     navbarMenu.classList.toggle("responsive");
+  
+   // Redirect to the desired page
+   window.location.href = "lists.html";
   }
